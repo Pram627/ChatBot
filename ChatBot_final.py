86d0a8e731a5b8c67c252e251b257e074e9df333
Pram627 <138636191+Pram627@users.noreply.github.com>
2023-11-19 15:28:46 +0530

    Update ChatBot_final.py

--- ChatBot_final.py
+++ ChatBot_final.py
@@ -1,23 +1,11 @@
-
-# for speech-to-text
 import speech_recognition as sr
-
-# for text-to-speech
 from gtts import gTTS
-
-# for language model
 import transformers
-
-import os
 import time
-
-# for data
 import os
 import datetime
 import numpy as np
 
-
-# Building the AI
 class ChatBot():
     def __init__(self, name):
         print("----- Starting up", name, "-----")
@@ -35,7 +23,6 @@ class ChatBot():
         except:
             print("Me  -->  ERROR")
 
-
     @staticmethod
     def text_to_speech(text):
         print("Dev --> ", text)
@@ -45,12 +32,10 @@ class ChatBot():
         statbuf = os.stat("res.mp3")
         mbytes = statbuf.st_size / 1024
         duration = mbytes / 200
-        os.system('start res.mp3')  # if you are using mac->afplay or else for windows->start
-        # os.system("close res.mp3")
+        os.system('start res.mp3')
+        os.system("close res.mp3")
         time.sleep(int(5.0*duration))
         os.remove("res.mp4")
-
-
 
     def wake_up(self, text):
         return True if self.name in text.lower() else False
@@ -62,25 +47,20 @@ class ChatBot():
 
 if __name__ == "__main__":
 
-# Running the AI
-
-    ai = ChatBot(name="dev")
+    ai = ChatBot(name="Dev")
     nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
     os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
-    ex =True
+    ex = True
     while ex:
         ai.speech_to_text()
 
-        ## wake up
         if ai.wake_up(ai.text) is True:
-            res = "Hello I am Dave the AI, what can I do for you?"
+            res = "Hello I am Dev the AI, what can I do for you?"
 
-        ## action time
         elif "time" in ai.text:
             res = ai.action_time()
 
-        ## respond politely
         elif any(i in ai.text for i in ["thank", "thanks"]):
             res = np.random.choice \
                 (["you're welcome!", "anytime!", "no problem!", "cool!", "I'm here if you need me!", "mention not"])
@@ -88,8 +68,8 @@ if __name__ == "__main__":
         elif any(i in ai.text for i in ["exit", "close"]):
             res = np.random.choice(["Tata", "Have a good day", "Bye", "Goodbye", "Hope to meet soon", "peace out!"])
 
-            ex =False
-        ## conversation
+            ex = False
+      
         else:
             if ai.text =="ERROR":
                 res = "Sorry, come again?"
